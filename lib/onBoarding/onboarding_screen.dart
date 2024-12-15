543d841eab3f06d4f2008188811aa23a45b4a3b2
Ahmed Essam <ae4418347@gmail.com>
2024-12-16 01:19:49 +0200

    enhancement Quran page

--- lib/onBoarding/onboarding_screen.dart
+++ lib/onBoarding/onboarding_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:introduction_screen/introduction_screen.dart';
 
-import 'home/home_screen.dart';
+import '../home/home_screen.dart';
 
 class OnboardingScreen extends StatelessWidget {
   static const String routeName = "/";
